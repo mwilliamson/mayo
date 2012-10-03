f256aa41e0652d60ece488b7ad9b611a0c8484d8
Michael Williamson <mike@zwobble.org>
2012-10-03 21:34:34 +0100

    Move finding of repositories to separate module

--- blah/commands.py
+++ blah/commands.py
@@ -2,39 +2,18 @@ import os
 import subprocess
 import sys
 
+import blah.finder
+
 def find_command(name):
     return commands[name]
 
 def what_is_this_command():
     directory = sys.argv[2] if len(sys.argv) > 2 else os.getcwd()
-    repository = find_repository(directory)
+    repository = blah.finder.find_repository(directory)
     if repository is None:
         print "Could not find source control repository"
     else:
         print "{0}+file://{1}".format(repository.type, repository.path)
-
-def find_repository(directory):
-    directory = os.path.abspath(directory)
-    while directory is not None:
-        files = os.listdir(directory)
-        if ".git" in files:
-            return Repository(os.path.join(directory, ".git"), "git")
-        
-        directory = parent(directory)
-        
-    return None
-
-class Repository(object):
-    def __init__(self, repo_path, repo_type):
-        self.path = repo_path
-        self.type = repo_type
-
-def parent(file_path):
-    parent = os.path.dirname(file_path)
-    if file_path == parent:
-        return None
-    else:
-        return parent
 
 commands = {
     "whatisthis": what_is_this_command,
---
+++ blah/finder.py
@@ -0,0 +1,24 @@
+import os
+
+def find_repository(directory):
+    directory = os.path.abspath(directory)
+    while directory is not None:
+        files = os.listdir(directory)
+        if ".git" in files:
+            return Repository(os.path.join(directory, ".git"), "git")
+        
+        directory = parent(directory)
+        
+    return None
+
+class Repository(object):
+    def __init__(self, repo_path, repo_type):
+        self.path = repo_path
+        self.type = repo_type
+
+def parent(file_path):
+    parent = os.path.dirname(file_path)
+    if file_path == parent:
+        return None
+    else:
+        return parent

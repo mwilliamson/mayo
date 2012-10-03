954b8ea1fcd64ae66ac28aa2d48cb9f71105cffb
Michael Williamson <mike@zwobble.org>
2012-10-03 22:28:21 +0100

    Factor out source control system definitions

--- blah/fetcher.py
+++ blah/fetcher.py
@@ -1,25 +1,36 @@
 import subprocess
 import os
 
+class Git(object):
+    def __init__(self):
+        self.name = "git"
+
+    def fetch(self, repository_uri, local_path):
+        if os.path.exists(local_path):
+            # TODO: check that local_path is a valid clone
+            subprocess.check_call(["git", "pull"], cwd=local_path)
+        else:
+            subprocess.check_call(["git", "clone", repository_uri, local_path])
+        
+
+class Hg(object):
+    def __init__(self):
+        self.name = "hg"
+
+    def fetch(self, repository_uri, local_path):
+        if os.path.exists(local_path):
+            # TODO: check that local_path is a valid clone
+            subprocess.check_call(["hg", "pull"], cwd=local_path)
+            subprocess.check_call(["hg", "update"], cwd=local_path)
+        else:
+            subprocess.check_call(["hg", "clone", repository_uri, local_path])
+
+systems = [Git(), Hg()]
+
 def fetch(repository_uri, local_path):
-    if repository_uri.startswith("git+"):
-        fetch_git_repository(repository_uri[len("git+"):], local_path)
-    elif repository_uri.startswith("hg+"):
-        fetch_hg_repository(repository_uri[len("hg+"):], local_path)
-    else:
-        raise RuntimeError("Source control system not recognised: " + repository_uri)
-
-def fetch_git_repository(repository_uri, local_path):
-    if os.path.exists(local_path):
-        # TODO: check that local_path is a valid clone
-        subprocess.check_call(["git", "pull"], cwd=local_path)
-    else:
-        subprocess.check_call(["git", "clone", repository_uri, local_path])
-
-def fetch_hg_repository(repository_uri, local_path):
-    if os.path.exists(local_path):
-        # TODO: check that local_path is a valid clone
-        subprocess.check_call(["hg", "pull"], cwd=local_path)
-        subprocess.check_call(["hg", "update"], cwd=local_path)
-    else:
-        subprocess.check_call(["hg", "clone", repository_uri, local_path])
+    for system in systems:
+        prefix = system.name + "+"
+        if repository_uri.startswith(prefix):
+            return system.fetch(repository_uri[len(prefix):], local_path)
+            
+    raise RuntimeError("Source control system not recognised: " + repository_uri)

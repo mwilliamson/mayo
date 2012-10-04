564ab9aab37131db8833bdc0328c0ec564e3aa9a
Michael Williamson <mike@zwobble.org>
2012-10-04 19:35:41 +0100

    Factor out common code in Git and Hg classes

--- blah/systems.py
+++ blah/systems.py
@@ -4,34 +4,37 @@ import subprocess
 from blah.repositories import Repository
 
 class Git(object):
-    def __init__(self):
-        self.name = "git"
-        self.vcs_directory = ".git"
-
-    def fetch(self, repository_uri, local_path):
-        if os.path.exists(local_path):
-            # TODO: check that local_path is a valid clone
-            subprocess.check_call(["git", "pull"], cwd=local_path)
-        else:
-            subprocess.check_call(["git", "clone", repository_uri, local_path])
-    
-    def repo(self, repo_path):
-        return Repository(repo_path, self.name)
+    def update(self, repository_uri, local_path):
+        subprocess.check_call(["git", "pull"], cwd=local_path)
+            
+    def clone(self, repository_uri, local_path):
+        subprocess.check_call(["git", "clone", repository_uri, local_path])
 
 class Hg(object):
-    def __init__(self):
-        self.name = "hg"
-        self.vcs_directory = ".hg"
+    def update(self, repository_uri, local_path):
+        subprocess.check_call(["hg", "pull"], cwd=local_path)
+        subprocess.check_call(["hg", "update"], cwd=local_path)
+        
+    def clone(self, repository_uri, local_path):
+        subprocess.check_call(["hg", "clone", repository_uri, local_path])
 
+class SourceControlSystem(object):
+    def __init__(self, name, fetcher):
+        self.name = name
+        self.vcs_directory = "." + name
+        self._fetcher = fetcher
+        
     def fetch(self, repository_uri, local_path):
         if os.path.exists(local_path):
             # TODO: check that local_path is a valid clone
-            subprocess.check_call(["hg", "pull"], cwd=local_path)
-            subprocess.check_call(["hg", "update"], cwd=local_path)
+            self._fetcher.update(repository_uri, local_path)
         else:
-            subprocess.check_call(["hg", "clone", repository_uri, local_path])
+            self._fetcher.clone(repository_uri, local_path)
             
     def repo(self, repo_path):
         return Repository(repo_path, self.name)
-        
-all_systems = [Git(), Hg()]
+
+all_systems = [
+    SourceControlSystem("git", Git()),
+    SourceControlSystem("hg", Hg())
+]

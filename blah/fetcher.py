003f932de484b46bb3553d4cba3a45de6a4ce959
Michael Williamson <mike@zwobble.org>
2012-10-04 19:26:15 +0100

    Split out system definitions to separate module

--- blah/fetcher.py
+++ blah/fetcher.py
@@ -1,34 +1,7 @@
-import subprocess
-import os
-
-class Git(object):
-    def __init__(self):
-        self.name = "git"
-
-    def fetch(self, repository_uri, local_path):
-        if os.path.exists(local_path):
-            # TODO: check that local_path is a valid clone
-            subprocess.check_call(["git", "pull"], cwd=local_path)
-        else:
-            subprocess.check_call(["git", "clone", repository_uri, local_path])
-        
-
-class Hg(object):
-    def __init__(self):
-        self.name = "hg"
-
-    def fetch(self, repository_uri, local_path):
-        if os.path.exists(local_path):
-            # TODO: check that local_path is a valid clone
-            subprocess.check_call(["hg", "pull"], cwd=local_path)
-            subprocess.check_call(["hg", "update"], cwd=local_path)
-        else:
-            subprocess.check_call(["hg", "clone", repository_uri, local_path])
-
-systems = [Git(), Hg()]
+from blah import systems
 
 def fetch(repository_uri, local_path):
-    for system in systems:
+    for system in systems.all_systems:
         prefix = system.name + "+"
         if repository_uri.startswith(prefix):
             return system.fetch(repository_uri[len(prefix):], local_path)
---
+++ blah/systems.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+class Git(object):
+    def __init__(self):
+        self.name = "git"
+
+    def fetch(self, repository_uri, local_path):
+        if os.path.exists(local_path):
+            # TODO: check that local_path is a valid clone
+            subprocess.check_call(["git", "pull"], cwd=local_path)
+        else:
+            subprocess.check_call(["git", "clone", repository_uri, local_path])
+        
+
+class Hg(object):
+    def __init__(self):
+        self.name = "hg"
+
+    def fetch(self, repository_uri, local_path):
+        if os.path.exists(local_path):
+            # TODO: check that local_path is a valid clone
+            subprocess.check_call(["hg", "pull"], cwd=local_path)
+            subprocess.check_call(["hg", "update"], cwd=local_path)
+        else:
+            subprocess.check_call(["hg", "clone", repository_uri, local_path])
+
+all_systems = [Git(), Hg()]

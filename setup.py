2e3987d1423495531a0a98fe7c6bb91102fcc2ed
Michael Williamson <mike@zwobble.org>
2012-10-06 14:45:07 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='blah',
-    version='0.1.1',
+    version='0.1.2',
     description='Thin wrapper around source control systems',
+    long_description=read("README.md"),
     author='Michael Williamson',
     url='http://github.com/mwilliamson/blah',
     scripts=["scripts/blah"],

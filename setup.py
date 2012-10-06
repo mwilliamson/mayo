07c7372ce8ed954be0ac7e332eee776f0239fb70
Michael Williamson <mike@zwobble.org>
2012-10-06 13:20:31 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='blah',
-    version='0.1',
+    version='0.1.1',
     description='Thin wrapper around source control systems',
     author='Michael Williamson',
     url='http://github.com/mwilliamson/blah',

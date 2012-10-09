28f800f7bf86947528153afeede47a0d54e191e5
Michael Williamson <mike@zwobble.org>
2012-10-09 22:20:18 +0100

    Expose blah.repository_at

--- blah/__init__.py
+++ blah/__init__.py
@@ -1,4 +1,4 @@
 from blah.fetcher import fetch
-from blah.repositories import find_repository
+from blah.repositories import find_repository, repository_at
 
-__all__ = ["fetch", "find_repository"]
+__all__ = ["fetch", "find_repository", "repository_at"]

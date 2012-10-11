3469a8436ad600aab908f3467b12d52c5a00c706
Michael Williamson <mike@zwobble.org>
2012-10-11 22:27:45 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='blah',
-    version='0.1.3',
+    version='0.1.4',
     description='Thin wrapper around source control systems',
     long_description=read("README"),
     author='Michael Williamson',

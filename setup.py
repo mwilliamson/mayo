1ef1a7159b196539a28986add765c90b1cec542e
Michael Williamson <mike@zwobble.org>
2013-03-23 11:22:13 +0000

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='mayo',
-    version='0.2.0',
+    version='0.2.1',
     description='Thin wrapper around source control systems',
     long_description=read("README"),
     author='Michael Williamson',

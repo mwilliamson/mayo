400f780e6c1f6f10dbc6fca8d9e0a09996c0bded
Michael Williamson <mike@zwobble.org>
2014-04-14 18:29:08 +0100

    Only require argparse on Python 2.6

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from distutils.core import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+_install_requires = [
+    "spur.local>=0.3.5,<0.4",
+]
+
+if sys.version_info[:2] <= (2, 6):
+    _install_requires.append("argparse>=1.1,<2.0")
 
 setup(
     name='mayo',
@@ -16,6 +25,6 @@ setup(
     url='http://github.com/mwilliamson/mayo',
     scripts=["scripts/mayo"],
     packages=['mayo'],
-    install_requires=["argparse==1.2.1", "spur.local>=0.3.5,<0.4"],
+    install_requires=_install_requires,
     keywords="source control vcs scm git mercurial hg",
 )
